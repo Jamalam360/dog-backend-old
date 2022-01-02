1eb12cffc7667dd3e9af19a0a4e591c28ba1fe4e
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-01-02 15:39:52 +0000

    debug

--- dogApi/dogApi.ts
+++ dogApi/dogApi.ts
@@ -1,5 +1,6 @@
 export const getRandomImage = async (): Promise<string> => {
   const req = await fetch("https://dog.jamalam.tech/api/v0/breeds/images/random");
+  console.log(req);
   const json = await req.json();
   return json.message;
 };
