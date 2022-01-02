1ac80a0d6f2a819ccba85d82dfcf312439cb5fb0
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-01-02 15:43:52 +0000

    fix error on Dog API startup

--- dogApi/dogApi.ts
+++ dogApi/dogApi.ts
@@ -1,6 +1,12 @@
+import { sleep } from "https://deno.land/x/sleep/mod.ts";
+
 export const getRandomImage = async (): Promise<string> => {
+  try {
   const req = await fetch("https://dog.jamalam.tech/api/v0/breeds/images/random");
-  console.log(req);
   const json = await req.json();
   return json.message;
+  } catch (_e) {
+    await sleep(2.5);
+    return getRandomImage();
+  }
 };
